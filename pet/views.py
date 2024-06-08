a0ef3d5c46e773b4a031eca7f330b1011c455851
emmanueldavids <emmanueldavids417@gmail.com>
2024-06-08 11:57:44 +0000

    added the user view in views.py

--- pet/views.py
+++ pet/views.py
@@ -3,18 +3,24 @@ from django.contrib.auth import login as auth_login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignUpForm
 from .forms import DonationForm
+from .models import Donation
+from django.core.paginator import Paginator
+
 
 
 
 
 from django.contrib import messages
 
+
 def index(request):
     # tasks = Task.objects.all()
     return render(request, 'index.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def contact(request):
     return render(request, 'contact.html')
@@ -25,23 +31,51 @@ def logout(request):
         logout(request)
         return redirect('index.html')
 
+
+# def dashboard(request):
+#     donations = Donation.objects.all().order_by('id')
+#     paginator = Paginator(dashboard, 10)  # Show 10 donations per page
+
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+
+#     context = {
+#         'page_obj': page_obj,
+#     }
+
+
+#     return render(request, 'dashboard.html', {'donations': donations}, {'page_obj':page_obj})
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    donations = Donation.objects.all() # Order by 'id' field to ensure consistency
+    paginator = Paginator(donations, 5)  # Show 10 donations per page
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+    }
+
+    return render(request, 'dashboard.html', context)
+    return render(request, 'dashboard.html', {'page_obj': page_obj})
+
+    return render(request, 'dashboard.html', {'donations': donations})
+
 
 def donate(request):
     if request.method == 'POST':
         form = DonationForm(request.POST)
         if form.is_valid():
             form.save()  # This will save the form data to the database
+            # messages.success(request, "Account was created for " + email)
             return redirect('dashboard')  # Redirect to a success page or wherever you want
     else:
         form = DonationForm()
     return render(request, 'donate.html', {'form': form})
 
-def donation_list(request):
-    donations = Donation.objects.all()
-    return render(request, 'donation_list.html', {'donations': donations})
 
+# def donation_list(request):
+    
 
 def register(request):
     if request.method == 'POST':
@@ -58,6 +92,7 @@ def register(request):
     else:
         form = SignUpForm()
     return render(request, 'register.html', {'form': form})
+
 
 
 def login(request):
