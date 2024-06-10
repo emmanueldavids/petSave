acc92ec4675524244ea0cf2ce76e0bf29ab41f4f
emmanueldavids <emmanueldavids417@gmail.com>
2024-06-10 14:21:02 +0000

    styling up my codes

--- petSave/settings.py
+++ petSave/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pet',
+    'rest_framework',
 ]
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
